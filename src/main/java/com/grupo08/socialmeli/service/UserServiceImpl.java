57b30149b3c59b749e775fda35c0e65404167df4
Miguel Angel Herrera Rodriguez <kingdocer@outlook.com>
2024-02-19 11:13:05 -0500

    Conflict resolves

--- src/main/java/com/grupo08/socialmeli/service/UserServiceImpl.java
+++ src/main/java/com/grupo08/socialmeli/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.grupo08.socialmeli.service;
 
 import com.grupo08.socialmeli.dto.response.FollowDto;
-import com.grupo08.socialmeli.dto.response.FollowersCountDto;
+import com.grupo08.socialmeli.dto.response.FollowersDto;
 import com.grupo08.socialmeli.entity.Buyer;
 import com.grupo08.socialmeli.entity.Seller;
 import com.grupo08.socialmeli.entity.User;
@@ -9,9 +9,9 @@ import com.grupo08.socialmeli.exception.BadRequestException;
 import com.grupo08.socialmeli.exception.NotFoundException;
 import com.grupo08.socialmeli.repository.IBuyerRepository;
 import com.grupo08.socialmeli.repository.ISellerRepository;
-import com.sun.jdi.IntegerValue;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -38,39 +38,43 @@ public class UserServiceImpl implements IUserService {
 
         if(seller.isEmpty()) throw new NotFoundException("No hay vendedor con ese ID.");
 
-        Optional<Seller> sellerToRemove = buyer.get().getFollowing().stream()
-                .filter(s -> s.getId() == idSeller)
-                .findFirst();
-
-        if (sellerToRemove.isPresent()) {
-            throw new BadRequestException("No puedes seguir un vendedor que ya sigues.");
-        }
-        
         buyer.get().addFollowingSeller(seller.get());
 
         return new FollowDto(idSeller, seller.get().getName());
     }
 
     @Override
-    public FollowersCountDto countSellerFollowers(int userId) {
-        //vars
-        Optional<Seller> seller = sellerRepository.findById(userId);
-        List<Seller> debug = List.of();
+    public void unfollow(int idBuyer, int idSeller) {
 
-        //validate: el usuario obtenido existe y es vendedor
-        if(seller.isEmpty()) throw new NotFoundException("El usuario no existe");
-        //validacion comentada dado repeticion de id entre compradores y vendedores
-        //if(buyerRepository.findById(userId).isPresent()) throw new BadRequestException("El id ingresado debe ser de un vendedor: se obtuvo comprador");
+        Optional<Buyer> buyer = buyerRepository.findById(idBuyer);
 
-        //process: obtener la lista de seguidos de todos los usuarios, filtrar los que coincidan con el id, hacer el recuento.
-        long followersCount = buyerRepository.findAll().stream()
-                .filter(comprador -> comprador.getFollowing().stream()
-                        .filter(vendedor -> ((Integer) vendedor.getId()).equals(userId)).isParallel()).toList().size();
+        Optional<Seller> seller = sellerRepository.findById(idSeller);
 
-        //Otra forma
-        List<Seller> sellersList =  buyerRepository.findAll().stream().map(Buyer::getFollowing).findAny().orElse(null);
-        long otherCount = sellersList.stream().filter(comprador -> comprador.getName().equals(seller.get().getName())).count();
-        //return
-        return new FollowersCountDto(userId, seller.get().getName(), followersCount);
+        if(buyer.isEmpty()) throw new NotFoundException("No se encuentra comprador con ese ID.");
+
+        if(seller.isEmpty()) throw new NotFoundException("No hay vendedor con ese ID.");
+
+        buyer.get().unFollowSeller(seller.get());
+
     }
+
+    public FollowersDto getFollowers(int idSeller) {
+        Optional<Seller> seller = sellerRepository.findById(idSeller);
+
+        if(seller.isEmpty()) throw new NotFoundException("No hay vendedor con ese ID.");
+
+        List<User> followers = seller.get().getFollowers();
+
+        FollowersDto followersDto = new FollowersDto(
+                seller.get().getId(),
+                seller.get().getName(),
+                followers.stream().map(this::convertFollowerDto).collect(Collectors.toList())
+        );
+        return followersDto;
+    }
+
+    private FollowDto convertFollowerDto(User user){
+        return new FollowDto(user.getId(), user.getName());
+    }
+
 }
